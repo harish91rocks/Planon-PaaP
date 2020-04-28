f207b240f602cc9245b97982f857cd0d1edc8f8b
sapunn <Srikanth.Punna@planonsoftware.com>
2020-04-28 08:06:37 +0000

    updated code

--- module/src/main/java/com/planonsoftware/tms/test/msg/bt/TestSX.java
+++ module/src/main/java/com/planonsoftware/tms/test/msg/bt/TestSX.java
@@ -1,9 +1,12 @@
 package com.planonsoftware.tms.test.msg.bt;
 
+import java.math.BigDecimal;
+
 import com.planonsoftware.platform.backend.businessrule.v3.IBusinessRule;
 import com.planonsoftware.platform.backend.businessrule.v3.IBusinessRuleContext;
 import com.planonsoftware.platform.backend.businesstransaction.v3.IBusinessTransactionService;
 import com.planonsoftware.platform.backend.connector.v2.messaging.IMessageConnectorService;
+import com.planonsoftware.platform.backend.connector.v2.messaging.out.IOutboundMessageBuilder;
 import com.planonsoftware.platform.backend.data.v1.IBusinessObject;
 
 public class TestSX implements IBusinessRule
@@ -12,8 +15,16 @@ public class TestSX implements IBusinessRule
 	public void execute(IBusinessObject aBO, IBusinessObject aOldBO, IBusinessRuleContext aContext) {
 		IBusinessTransactionService businessTransactionService =  aContext.getBusinessTransactionService();
         businessTransactionService.execute(new TestBusinessTransactionExecuter(aBO));
-        
+
         IMessageConnectorService messageConnectorService = aContext.getMessageConnectorService();
-        messageConnectorService.createNewOutboundMessageBuilder();
+        IOutboundMessageBuilder  outboundMessageBuilder= messageConnectorService.createNewOutboundMessageBuilder();
+        outboundMessageBuilder.addAdditionalInfo("info1", "Value1");
+        outboundMessageBuilder.addStringFieldByName("field1", "field1");
+        outboundMessageBuilder.addIntegerFieldByName("field2", 11);
+        outboundMessageBuilder.addBooleanFieldByName("field3", true);
+        outboundMessageBuilder.addBigDecimalFieldByName("field4", BigDecimal.ONE);
+
+        outboundMessageBuilder.buildAndSave();
+        
 	}
 }
